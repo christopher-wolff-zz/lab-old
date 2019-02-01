5101cfaf8d2287cd1d0f5d721176cd1dde218088
Christopher Wolff <cwolff98@gmail.com>
2019-02-01 18:29:49 -0500

    Rerun notebooks

--- lab/utils/plotting.py
+++ lab/utils/plotting.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def smooth(x, window_length):
+def smooth(x, window_length, method='mirror'):
     """Smooth an array of time series data.
 
     We use a moving average filter and extend the boundary values of `x` so that
@@ -12,6 +12,8 @@ def smooth(x, window_length):
     Args:
         x: (ndarray) A one-dimensional array to be smoothed.
         window_length: (int) The length of the smoothing window.
+        method: (str) The method with which to replicate boundary points before
+            smoothing. Either 'mirror' or 'same'.
 
     Returns:
         (ndarray) The smoothed array.
@@ -21,8 +23,14 @@ def smooth(x, window_length):
     num_right_vals = window_length // 2
     if window_length % 2 == 0:
         num_left_vals -= 1
-    x_left = np.ones(num_left_vals) * x[0]
-    x_right = np.ones(num_right_vals) * x[-1]
+    if method == 'same':
+        x_left = np.ones(num_left_vals) * x[0]
+        x_right = np.ones(num_right_vals) * x[-1]
+    elif method == 'mirror':
+        x_left = x[num_left_vals-1::-1]
+        x_right = x[-num_right_vals:]
+    else:
+        raise ValueError(f'Unknown method: {method}')
     x_extended = np.r_[x_left, x, x_right]
     window = np.ones(window_length, 'd') / window_length
     return np.convolve(x_extended, window, mode='valid')
